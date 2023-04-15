a030729265c17dd37868178ee8adc78a5c9f2826
hamidehparsa <118659274+hamidehparsa@users.noreply.github.com>
2023-04-15 11:25:06 +0430

    added github link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <Search />
+      <footer>
+        <a href="https://github.com/hamidehparsa/weather-react" target="_blank">
+          Open-source code
+        </a>
+        by Hamideh Parsaeian
+      </footer>
     </div>
   );
 }
